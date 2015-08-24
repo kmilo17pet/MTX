50cfdd9445807ef97955574a860b7df56c77b1b3
Eng. Juan Camilo Gómez Cadavid MSc. <kmilo17pet@gmail.com>
2015-08-24 11:36:48 -0500

    updated documentation with some examples

--- main.c
+++ main.c
@@ -1,40 +1,32 @@
-/* solving the matrix equation A*x=b using MTX */
+/* 
+ * Evaluate the expresion X= (A*B)/(alpha + B'*A*B) 
+ *  A is a 4-square random matrix and B is a 4-column random matrix
+ * alpha is scalar
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include "mtx.h"
 
 int main(void){
-    double a[3][3]= {
-                    3.1, 1.3,-5.7,
-                    1.0, -6.9, 5.8,
-                    3.4, 7.2, -8.8
-                    };
-    double b[2][2]= {
-                    0.0357,    0.9340,
-                    0.8491,    0.6787 
-                    };
-    double c[3][2]={
-                    0.7577,    0.6555,
-                    0.7431,    0.1712,
-                    0.3922,    0.7060,
-                    };
-    double d[3][1]={
-                    1,
-                    2,
-                    3
-                    };
-    matrix A = mtx_2dtomtx(a);
-    matrix B = mtx_2dtomtx(b);
-    matrix C = mtx_2dtomtx(c);
-    matrix D = mtx_2dtomtx(d);
-    matrix X = NULL;
-    matrix Y = NULL;
-    do{
-        X = mtx_sylvester(A,B,C);
-        mtx_del(X);
-    }while(1);
+    /*Define the  using the matrix notation*/
+    matrix A = mtx_rand(4,4);
+    matrix B = mtx_rand(4,1);
+    matrix X = NULL; //holds the expression
+    matrix AB = NULL; //holds the auxiliar A*B result
+    matrix BtAB = mtx_new(1,1); // holds the auxiliar B'*A*B result 
+    double alpha = 0.8;
+    
+    /*Compute the operations*/
+    AB = mtx_prod(1.0, A,B); // AB = A*B
+    mtx_dgemm(1.0, B, 't', AB, '.', 0, BtAB); // compute B'*A*B using the generalized matrix product routine mtx_dgemm
+    X = mtx_koper(AB, '/', (alpha + BtAB->pos[0][0]) );
+    /*Display the matrices*/
+    mtx_dispn(A,B,AB,BtAB,X);
+    /*Release the heap*/
     mtx_del(A);
     mtx_del(B);
-    mtx_del(C);
+    mtx_del(AB);
+    mtx_del(BtAB);
+    mtx_del(X);
     return EXIT_SUCCESS;
 }
